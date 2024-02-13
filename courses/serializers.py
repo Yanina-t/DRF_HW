08c5b9a1fdf5dc461b2ddc189ff7f25e86b277dc
yanina <vyaninat@gmail.com>
2024-02-13 21:24:37 +0100

    #24.2  #2 1)adding migration 0002_payment.py (not added to git),2) added a separate serializer to reduce lesson information

--- courses/serializers.py
+++ courses/serializers.py
@@ -12,17 +12,19 @@ class LessonSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'description', 'preview', 'video_link', 'course']
 
 
+class SimpleLessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ['id', 'title', 'video_link']
+
+
 class CourseSerializer(serializers.ModelSerializer):
     num_of_lessons = serializers.SerializerMethodField()
-    lessons = LessonSerializer(many=True, read_only=True)
+    lessons = SimpleLessonSerializer(many=True, read_only=True)
 
     def get_num_of_lessons(self, obj):
         return obj.lessons.count()
 
-
     class Meta:
         model = Course
         fields = ['id', 'title', 'preview', 'description', 'num_of_lessons', 'lessons']
-
-
-
